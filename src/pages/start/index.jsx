8d567892dea6f4b13144d685bf5f879448b8efbf
Ricardo Lopes <ricardolopesroj@gmail.com>
2022-11-28 20:12:10 -0300

    estelização com css

--- src/pages/start/index.jsx
+++ src/pages/start/index.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 import { View } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { ScrollView, StyleSheet } from "react-native-gesture-handler";
 
 export default function Start() {
 
   return(
-    <View>
+    <View style={styles.container}>
       <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Image source={require('../../assets/icons/logo3.png')} />
-          <Text>
+        <View style={{alignItems: 'center'}}>
+          <Image source={require("../../assets/icons/logo3.png")} style={styles.logo}/>
+          <Text style={styles.description}>
             Vamos transformar sua vida {'/n'} em jogo, buscando {'/n'} sempre o melhor nivel.
           </Text>
         </View>
       </ScrollView>
     </View>
-    
+   
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundcolor: 'rgba(21,21,21, 0.98)',
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: '#FFFFFF',
+    fontSize: ''
+  }
+});
