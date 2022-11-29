6f87ff3faaf4e3413fa781b7c51864c9fa5c2c3a
Ricardo Lopes <ricardolopesroj@gmail.com>
2022-11-28 22:15:06 -0300

    criando botao de adicionar habito

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,17 @@
 import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 
 import LifeStatus from "../../components/common/lifeStatus";
+import CreateHabit from "../../components/home/createHabit/index,";
 import StatusBar from "../../components/home/statusBar";
 
 export default function Home() {
   const navigation = useNavigation();
+  const [mindHabit, setMindHabit] = useState();
+  const [moneyHabit, setMoneyHabit] = useState();
+  const [bodyHabit, setBodyHabit] = useState();
+  const [funHabit, setFunHabit] = useState();
 
   function handleNavExplanation() {
     navigation.navigate("AppExplanation");
@@ -19,6 +24,10 @@ export default function Home() {
           <Text style={styles.dailyChecks}>❤️ 20 dias - ✔️ 80 checks</Text>
           <LifeStatus />
           <StatusBar />
+          <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
+          <CreateHabit habitArea="Financeiro" borderColor="#85BB65" />
+          <CreateHabit habitArea="Corpo" borderColor="#FF0044" />
+          <CreateHabit habitArea="Humor" borderColor="#FE7F23" />
         </View>
         <Text style={styles.explanationText}
           onPress={() => {handleNavExplanation();}}> Ver explicação novamente</Text>
