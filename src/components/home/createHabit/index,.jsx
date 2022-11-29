616dbbb129d8d9e217b4bb673d768880c16abba5
Ricardo Lopes <ricardolopesroj@gmail.com>
2022-11-28 22:41:43 -0300

    edição e criação dos habitos

--- src/components/home/createHabit/index,.jsx
+++ src/components/home/createHabit/index,.jsx
@@ -12,7 +12,7 @@ export default function CreateHabit({ habitArea, borderColor}) {
       style={[styles.button, {borderColor: borderColor}]}
       onPress={handleCreate}>
         <Text style={styles.habitTitle}>Adicionar meta 
-          {habitArea === 'Mente' ? 'da' : 'do'} {habitArea}
+          {habitArea === 'Mente' ? ' da' : ' do'} {habitArea}
         </Text>
     </TouchableOpacity>
   )
